59b1f2a7e0d1979688f3167c8d24c8a11c552ba4
Dalia Sawaya <dalia.sawaya@flatironschool.com>
2020-07-15 17:53:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,17 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1 
+      element + "s"
+    else
+      element
+    end
+  end
+end
